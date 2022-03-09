8950e2dc45e9d8749ab0b9a431a8c36c26cc3c4d
Miguel Rivera Robles <al221910331@gmail.com>
2022-03-08 22:23:01 -0600

    Crud Usuarios

---
+++ app/Models/Usuarios.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuarios extends Model
+{
+    use HasFactory;
+    protected $table ="usuarios";
+    protected $primaryKey ="id";
+    protected $fillable =[
+        'nombreCompleto',
+        'telefono',
+        'correo',
+        'password',
+        'estatus',
+        'id_tipo',
+        'img'
+    ];
+}
